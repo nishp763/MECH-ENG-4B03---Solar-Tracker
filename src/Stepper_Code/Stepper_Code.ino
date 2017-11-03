4273449255e1496261dc4c926aeb26e91e1f05be
nishp763 <nishp763@gmail.com>
2017-11-02 22:42:17 -0400

    Accurately got to move 180 degrees

--- src/Stepper_Code/Stepper_Code.ino
+++ src/Stepper_Code/Stepper_Code.ino
@@ -23,6 +23,7 @@ void setup()
   // Initialize Azimuth Stage Opto Sensor as INPUT
   pinMode(Azimuth_Optical_Sensor, INPUT);
   home_azimuth();
+  //move_azimuth("CW",3600);
 }
 
 void loop() 
@@ -36,6 +37,9 @@ void home_azimuth() // This function will place the Azimuth stage to the home po
   Serial.println("Moving Azimuth Stage to the Home Position");
   for (int i = 0; i <= 180; i++)
   {
+    Serial.print("i > ");
+    Serial.print(i);
+    Serial.println("");
   	if(digitalRead(Azimuth_Optical_Sensor) == HIGH) // Sensor Triggered
 	  {
 	  	Serial.println("Azimuth Stage = Home Position");
@@ -43,24 +47,31 @@ void home_azimuth() // This function will place the Azimuth stage to the home po
 	  }
 	  else
 	  {
-	  	move_azimuth("CW", 10); // Move the Azimuth Stage
+	  	move_azimuth("CW", 1); // Move the Azimuth Stage with a precision of 1 degree
 	  }
   }
   Serial.println("ERROR: Aziumth Stage moved from 0 - 180 degrees CW, Home Position not reached. Try cleaning the optical sensor or perhaps go in CCW position.");
 }
 
-void move_azimuth(String motor_direction, int steps) // This function will send a signal to the stepper motor to move the azimuth stage
+void move_azimuth(String motor_direction, int azimuth_move_degrees) // This function will send a signal to the stepper motor to move the azimuth stage
 {
-	Serial.println("Stepping Azimuth Motor...");
+  /* Convert degrees to the # of steps */
+  int azimuth_steps = azimuth_move_degrees/0.05;
+  Serial.print("Azimuth Steps > ");
+  Serial.print(azimuth_steps);
+  Serial.println("");
+ // Serial.println("Moving Azimuth Motor to " + azimuth_move_degrees + " degrees with " + azimuth_steps + " steps.");
 
   /* Set Direction PIN on Azimuth Stage */
   if (motor_direction == "CW")
   {
     digitalWrite(Azimuth_Motor_Direction, LOW); // LOW = Clock Wise direction
+    Serial.println("Azimuth Motor Direction -> Clockwise");
   }
   else if (motor_direction == "CCW")
   {
     digitalWrite(Azimuth_Motor_Direction, HIGH); // HIGH = Counter Clock Wise direction
+    Serial.println("Azimuth Motor Direction -> Counter Clockwise");
   }
   else
   {
@@ -69,11 +80,12 @@ void move_azimuth(String motor_direction, int steps) // This function will send 
   }
 
   /* Send Clock Signals to the Stepper Motor to move in the direction specified */
-  for(int i = 0; i < steps; i++)
+  for(int i = 0; i < azimuth_steps; i++)
   {
     digitalWrite(Azimuth_Motor_Clock, HIGH);
     delay(Stepper_Motor_Delay);
     digitalWrite(Azimuth_Motor_Clock, LOW);
     delay(Stepper_Motor_Delay);
   }
+  delay(500);
 }
