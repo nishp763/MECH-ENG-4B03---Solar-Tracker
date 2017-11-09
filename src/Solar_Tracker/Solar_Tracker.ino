dace3be9a1436bf4acea5d0d9413d3e383997bd1
nishp763 <nishp763@gmail.com>
2017-11-08 23:09:15 -0500

    Changed filed names from Stepper.ino to Solar_Tracker.ino

--- src/Solar_Tracker/Solar_Tracker.ino
+++ src/Solar_Tracker/Solar_Tracker.ino
@@ -1,155 +1,91 @@
-#include <Wire.h>
-//#include
-//#include
-//#include
-#define ASCIIZERO 48 // '0' = 48
-#define pi 3.1415926535897932384
-#define twoPI (2.0*pi)
-#define rad (pi/180.0)
-#define dEarthRadius 6371.01 // in km
-#define dAstroUnit 149597890 // in km
-#define OPTOPINAZ 2
-#define OPTOPINZE 3
-#define MOTZECWCCW 5
-#define MOTZECLK 4
-#define MOTAZCWCCW 7
-#define MOTAZCLK 6
-#define MAXAZHOMESTEP 6250
-#define MAXZEHOMESTEP 10000 // ZE home max subject to revision
-#define STEPSAZHOME 1800 // AZ home position = East = +90 degrees = 1800 steps ***Update for final version => Home = 22.5 degrees***
-#define ANGLEZEHOME 90.0 // ZE home position = Horizon = +90 degrees
-#define STEPDLY 5
-#define ZENITHHOMENUTPOSITION 24
-#define STEPSPERDEGAZ 20.0 // 1.8 deg per step and 36:1 reduction worm gears
-#define STEPSPERMMZE 100 // temporary demo
-//Latitude and Longitude for McMaster (JHE) = 43.260181 (N), 79.920892 (W). Latitude is considered positive to the North and longitude to the East.
-//Use decimal format (Latitude = 43 + 26.0181/60 = 43.434; Longitude = -1 * (79 degrees + 92.0892/60) = -80.535;)
-const double MCMASTERLATITUDE = 43.434;
-const double MCMASTERLONGITUDE = -80.535;
-
-
-//azimuth angle should be between 0-180 degrees
+#define Azimuth_Motor_Direction 7 // LOW = CW , HIGH = CCW
+#define Azimuth_Motor_Clock 6
+#define Azimuth_Optical_Sensor 2 // HIGH = Triggered
+#define Stepper_Motor_Delay 5
 
 
 
-//****************************************************************************************************************************************************************
 void setup()
- {
-  // setup serial communication
-  Serial.begin(9600);
+{
+  Serial.begin(9600); // initialize the serial port
+
+  /* Print message on the serial monitor */
   Serial.println("SolarTracker v4.4");
-  Serial.println("Serial Connection initalized");
+  Serial.println("Serial Connection Initalized");
   Serial.println("");
-  pinMode(MOTAZCWCCW,OUTPUT); // AZ motor
-  pinMode(MOTAZCLK, OUTPUT);
-  pinMode(OPTOPINAZ, INPUT); // opto slot sensor AZ
-  //digitalWrite(MOTAZCWCCW,HIGH); // always go home CCW = HIGH
-  pinMode(MOTZECWCCW,OUTPUT); // EL motor
-  pinMode(MOTZECLK, OUTPUT);
-  pinMode(OPTOPINZE, INPUT); // opto slot sensor ZE
-  //digitalWrite(MOTZECWCCW,HIGH); // always go home CCW = HIGH
-  Serial.println("");
-  // home the AZ stepper by looking for blocked opto slot, when home = East = 90 degrees = 1800 steps
-  // home the Azimuth stepper
-  homeAzimuth();
-  trackSun();
 
-  
-} // end setup()
+  /* Initialize all Sensors and Actuators for the Azimuth Stage */
 
-//****************************************************************************************************************************************************************
-void loop(){
-  int iCount;
-  Serial.println("loop");
-  delay(2000);
-  int optsensAZ;
-  int optsensZE;
-  optsensAZ = digitalRead(OPTOPINAZ);
-  optsensZE = digitalRead(OPTOPINZE);
-  if (optsensAZ == HIGH){
-    Serial.println("AZ triggered");
-    }
-  if (optsensZE == HIGH){
-    Serial.println("ZE triggered");
-    }
-  
+  // Initialize Azimuth Stage Motors as OUTPUT
+  pinMode(Azimuth_Motor_Direction, OUTPUT);
+  pinMode(Azimuth_Motor_Clock, OUTPUT);
+
+  // Initialize Azimuth Stage Opto Sensor as INPUT
+  pinMode(Azimuth_Optical_Sensor, INPUT);
+  home_azimuth();
+  //move_azimuth("CW",3600);
 }
 
-//****************************************************************************************************************************************************************
+void loop() 
+{
+  // put your main code here, to run repeatedly:
 
+}
 
+void home_azimuth() // This function will place the Azimuth stage to the home position
+{
+  Serial.println("Moving Azimuth Stage to the Home Position");
+  for (int i = 0; i <= 180; i++)
+  {
+    Serial.print("i > ");
+    Serial.print(i);
+    Serial.println("");
+  	if(digitalRead(Azimuth_Optical_Sensor) == HIGH) // Sensor Triggered
+	  {
+	  	Serial.println("Azimuth Stage = Home Position");
+	  	return;
+	  }
+	  else
+	  {
+	  	move_azimuth("CW", 1); // Move the Azimuth Stage with a precision of 1 degree
+	  }
+  }
+  Serial.println("ERROR: Aziumth Stage moved from 0 - 180 degrees CW, Home Position not reached. Try cleaning the optical sensor or perhaps go in CCW position.");
+}
 
-void trackSun(){
-  //might want to change parameter to degrees instead of steps for stepAzimuth and stepZenith
-  stepAzimuth("CW", 2700); //45 degrees b/c 1 step is 0.05 for azimuth because 1 step is 1.8 degrees of the worm drive which has a GR of 36
-  stepZenith("CCW", 20); //20 steps * 1.8 = 36 degrees
+void move_azimuth(String motor_direction, int azimuth_move_degrees) // This function will send a signal to the stepper motor to move the azimuth stage
+{
+  /* Convert degrees to the # of steps */
+  int azimuth_steps = azimuth_move_degrees/0.05;
+  Serial.print("Azimuth Steps > ");
+  Serial.print(azimuth_steps);
+  Serial.println("");
+ // Serial.println("Moving Azimuth Motor to " + azimuth_move_degrees + " degrees with " + azimuth_steps + " steps.");
+
+  /* Set Direction PIN on Azimuth Stage */
+  if (motor_direction == "CW")
+  {
+    digitalWrite(Azimuth_Motor_Direction, LOW); // LOW = Clock Wise direction
+    Serial.println("Azimuth Motor Direction -> Clockwise");
+  }
+  else if (motor_direction == "CCW")
+  {
+    digitalWrite(Azimuth_Motor_Direction, HIGH); // HIGH = Counter Clock Wise direction
+    Serial.println("Azimuth Motor Direction -> Counter Clockwise");
+  }
+  else
+  {
+    Serial.println("ERROR: motor direction not specified properly...setting Azimuth to CW");
+    digitalWrite(Azimuth_Motor_Direction, LOW); // LOW = Clock Wise direction
   }
 
-//****************************************************************************************************************************************************************
-void homeAzimuth(){
-  Serial.println("Moving the azimuth to the home position");
-  int i;
-  int optsensAZ;
-  for (i = 0; i <= 180; i++){
-    optsensAZ = digitalRead(OPTOPINAZ);
-    if (optsensAZ == HIGH){
-      Serial.println("***AZIMUTH OPTICAL SENSOR TRIGGERED***");
-      break;
-      }
-
-    stepAzimuth("CW", 10);
-    
-    }
-  Serial.println("Azimuth angle moved 180 degrees without reaching optical sensor.");  
+  /* Send Clock Signals to the Stepper Motor to move in the direction specified */
+  for(int i = 0; i < azimuth_steps; i++)
+  {
+    digitalWrite(Azimuth_Motor_Clock, HIGH);
+    delay(Stepper_Motor_Delay);
+    digitalWrite(Azimuth_Motor_Clock, LOW);
+    delay(Stepper_Motor_Delay);
   }
-
-//****************************************************************************************************************************************************************
-
-void stepAzimuth(String dir, int steps){
-  Serial.println("Stepping Azimuth motor...");
-  int i;
-  if (dir == "CW"){
-    digitalWrite(MOTAZCWCCW, LOW);
-    }
-  else if (dir == "CCW"){
-    digitalWrite(MOTAZCWCCW, HIGH);
-    }
-
-  else{
-    Serial.println("ERROR: direction not specified properly... setting zenith to CW");
-    digitalWrite(MOTAZCWCCW, LOW);
-    }
-    
-  for (i = 0; i < steps; i++){     // STEP 1.8 DEG (with 36 reduction = 0.05 deg) 
-    Serial.println(i);
-    digitalWrite(MOTAZCLK, HIGH);
-    delay(STEPDLY);
-    digitalWrite(MOTAZCLK, LOW);
-    delay(STEPDLY);
-    }
-  }
-  
-//******************************************************************************************************************************************************************
-void stepZenith(String dir, int steps){
-  Serial.println("Stepping Zenith motor...");
-  int i;
-  if (dir == "CW"){
-    digitalWrite(MOTZECWCCW, LOW);
-    }
-  else if (dir == "CCW"){
-    digitalWrite(MOTZECWCCW, HIGH);
-    }
-
-  else{
-    Serial.println("ERROR: direction not specified properly... setting azimuth to CW");
-    digitalWrite(MOTZECWCCW, LOW);
-    }
-    
-  for (i = 0; i < steps; i++){     // STEP 1.8 DEG (with 36 reduction = 0.05 deg)
-    Serial.println(i);
-    digitalWrite(MOTZECLK, HIGH);
-    delay(STEPDLY);
-    digitalWrite(MOTZECLK, LOW);
-    delay(STEPDLY);
-    }
-  }
+  delay(500);
+}
